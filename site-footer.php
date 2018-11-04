65e101fea52595e756f96d32d6f2cfe9a0e36f6a
Carsten Christiansen <keisen@stofanet.dk>
2018-11-04 16:53:05 +0100

    Afleveret version i delopgave 6

--- site-footer.php
+++ site-footer.php
@@ -6,9 +6,9 @@
       Siden 2010 har vejrportalen.dk bragt vores kunder på forkant med vejret.<br>
       Vi bringer udsigter, varsler og meldinger for dit lokalområde – til dig.<br>
       Få besked på SMS, på mail og ved personligt opkald når <strong>DU</strong> ønsker det<br>
-      <a href="https://designguide.lf.dk/grundelementer/farver">farveskema</a><br>
+    <!--  <a href="https://designguide.lf.dk/grundelementer/farver">farveskema</a><br>
       <a href="http://blog.koalite.com/bbg/">Overwrite Bootstrap btn</a><br>
-      <a href="https://40.127.170.50/vejrportalen/#">andet eksempel</a><br>
+      <a href="https://40.127.170.50/vejrportalen/#">andet eksempel</a><br>  -->
     </p>
   </div>
   
